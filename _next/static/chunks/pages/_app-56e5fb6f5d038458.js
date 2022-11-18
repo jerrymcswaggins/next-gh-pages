40435f1da477d98607a516e8c029333743d31739
Ayresia <nalizyoncatania0@gmail.com>
2022-11-18 10:26:08 +0100

    Deploy

---
+++ _next/static/chunks/pages/_app-56e5fb6f5d038458.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{888:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(8224)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(888),_(3225)}),_N_E=n.O()}]);
